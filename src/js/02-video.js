6f31ca63dbc1eab526dfd8a89c9798ad61d65625
ant0nvasyliev <anton12vasyliev@gmail.com>
2023-06-17 15:03:20 +0100

    1

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,21 @@
+import Player from '@vimeo/player';
+import throttle from 'lodash.throttle';
 
+const iframe = document.getElementById('vimeo-player');
+const player = new Player(iframe);
+
+const saveTimeThrottled = throttle(function (currentTime) {
+  localStorage.setItem('videoplayer-current-time', currentTime);
+}, 1000);
+
+player.on('timeupdate', function (data) {
+  const currentTime = data.seconds;
+  saveTimeThrottled(currentTime);
+});
+
+window.addEventListener('DOMContentLoaded', function () {
+  const savedTime = localStorage.getItem('videoplayer-current-time');
+  if (savedTime) {
+    player.setCurrentTime(savedTime);
+  }
+});
--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,39 @@
+import throttle from 'lodash.throttle';
 
+document.addEventListener('DOMContentLoaded', function () {
+  const form = document.querySelector('.feedback-form');
+  const emailInput = form.querySelector('input[name="email"]');
+  const messageInput = form.querySelector('textarea[name="message"]');
+
+  // Перевірка стану сховища під час завантаження сторінки
+  const savedState = JSON.parse(localStorage.getItem('feedback-form-state'));
+  if (savedState) {
+    emailInput.value = savedState.email;
+    messageInput.value = savedState.message;
+  }
+
+  // Функція для збереження стану форми в локальне сховище
+  const saveFormState = throttle(function () {
+    const currentState = {
+      email: emailInput.value,
+      message: messageInput.value,
+    };
+    localStorage.setItem('feedback-form-state', JSON.stringify(currentState));
+  }, 500);
+
+  // Відстежування події input і збереження стану форми
+  emailInput.addEventListener('input', saveFormState);
+  messageInput.addEventListener('input', saveFormState);
+
+  // Очищення сховища та полів форми під час сабміту форми
+  form.addEventListener('submit', function (event) {
+    event.preventDefault();
+    console.log({
+      email: emailInput.value,
+      message: messageInput.value,
+    });
+    localStorage.removeItem('feedback-form-state');
+    emailInput.value = '';
+    messageInput.value = '';
+  });
+});
